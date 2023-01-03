15debe80d3521d5454c93456286a6d08dd207537
zachary-o <104291766+zachary-o@users.noreply.github.com>
2023-01-03 21:41:36 +0200

    Add files via upload

---
+++ Frequently Asked Questions/app.js
@@ -0,0 +1,12 @@
+// Variable
+
+const accordion = document.getElementsByClassName('content-container');
+
+for (i = 0; i < accordion.length; i++) {
+
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    })
+
+};
+
